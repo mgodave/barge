4fc1c4a599cf6eabfe15dc9d97c205be0d7d2816
gfluckiger <gfluckiger@murex.com>
2015-07-10 18:35:32 +0200

    Remove erroneous copyright statement

--- barge-jax-rs/src/main/java/pack.java
+++ barge-jax-rs/src/main/java/pack.java
@@ -1,10 +1,3 @@
-/**
- *  Copyright Murex S.A.S., 2003-2014. All Rights Reserved.
- *
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
- */
-
 import com.google.common.io.ByteStreams;
 
 import java.io.*;
